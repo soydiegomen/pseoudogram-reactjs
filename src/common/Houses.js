2252c3e94f162a669d6d72d7a6a112b7ea21e612
Diego Mendoza <dmendoza@inflexionsoftware.com>
2018-02-21 22:38:34 -0600

    Agregue llamadas a servicios de tipo POST y PUT para hacer pruebas de consumo de servicios desde React.

--- src/common/Houses.js
+++ src/common/Houses.js
@@ -4,10 +4,17 @@ class Houses extends Component {
 
 	constructor(props) {
     	super(props)
-        this.state = { listHouses: [] }
+        this.state = { 
+        	listHouses: [],
+        	resultCreateUser: null
+        }
+
+        this.getHouses = this.getHouses.bind(this);
+        this.handleCreateUser = this.handleCreateUser.bind(this);
+        this.handleUpdateUser = this.handleUpdateUser.bind(this);
     }
 
-	componentWillMount() {
+    getHouses() {
 		fetch('http://localhost:3000/api/houses/byStatus/Publicado')
 		.then((response) => {
 
@@ -17,12 +24,90 @@ class Houses extends Component {
 			this.setState( { listHouses: listHouses });
 		})
 	}
+
+	handleCreateUser() {
+		fetch('http://localhost:3000/api/users', {
+		  method: 'POST',
+		  headers: {
+		    Accept: 'application/json',
+		    'Content-Type': 'application/json',
+		  },
+		  body: JSON.stringify({
+		    name: 'Luis Gomez3',
+			email: 'lgomez3@mail.com',
+			password: '1234',
+			userType: 'User',
+			isActive: 'true'
+		  }),
+		})
+		.then((response) => {
+			if(response.status == 200){
+				return response.json()
+			}else{
+				throw 'Error on save. Posiblemente el email este duplicado';
+			}
+		})
+		.then((result) => {
+			console.log('Post service result: ', result);
+			this.setState( { resultCreateUser: 'Usuario creado satisfactoriamente id:' + 
+				result._id });
+		})
+		.catch((error) => {
+	      console.error(error);
+	      this.setState( { resultCreateUser: error });
+	    });
+	}
+
+	handleUpdateUser() {
+		fetch('http://localhost:3000/api/user/5a8e47da1b92a6037ae9ad12', {
+		  method: 'PUT',
+		  headers: {
+		    Accept: 'application/json',
+		    'Content-Type': 'application/json',
+		  },
+		  body: JSON.stringify({
+		    name: 'Luis Gomez3',
+			email: 'supermailYY@mail.com',
+			password: '1234',
+			userType: 'User',
+			isActive: 'true'
+		  }),
+		})
+		.then((response) => {
+			if(response.status == 200){
+				return response.json()
+			}else{
+				throw 'Error on update. Posiblemente el email este duplicado';
+			}
+		})
+		.then((result) => {
+			console.log('Post service result: ', result);
+			this.setState( { resultCreateUser: 'Usuario creado satisfactoriamente id:' + 
+				result._id });
+		})
+		.catch((error) => {
+	      console.error(error);
+	      this.setState( { resultCreateUser: error });
+	    });
+	}
+
+	componentWillMount() {
+		this.getHouses();		
+	}
+
 	render() {
 		
 
     	return (
     		<div className="App">
 	      		<h1>Houses!</h1>
+	      		<div>
+	      			<button onClick={this.handleCreateUser}>Crear usuario</button>
+	      			<button onClick={this.handleUpdateUser}>Actualizar usuario</button>
+	      			<br/>
+	      			<b>Result: </b><span>{this.state.resultCreateUser}</span>
+	      		</div>
+	      		<br/>
 	      		<ul>
 	      			{this.state.listHouses.map((house) => {
 						return (<li><b>{house.title}</b><span> - {house.propertyType}</span></li>);
